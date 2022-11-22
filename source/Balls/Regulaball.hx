179d2174b9595a13857f5ad0e8d0311b75fca061
gameralo <115435685+gameralo@users.noreply.github.com>
2022-11-22 16:21:33 -0500

    Update Regulaball.hx

--- source/Balls/Regulaball.hx
+++ source/Balls/Regulaball.hx
@@ -1,5 +1,18 @@
-hx
 package;
 
-import flixel.text.FlxText;
-import flixel.FlxState;
+import flixel.FlxSprite;
+import flixel.FlxG;
+
+class MySprite extends FlxSprite
+{
+    public function new()
+    {
+        super();
+    }
+
+    override public function update(elapsed:Float):Void
+    {
+      //Insert Ball here
+        super.update(elapsed);
+    }
+}
